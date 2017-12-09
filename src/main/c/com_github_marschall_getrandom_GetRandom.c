9b68eb766d78678e2e8ace11a9b3f293e5f779c0
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-09 18:51:18 +0100

    start buffer management

--- src/main/c/com_github_marschall_getrandom_GetRandom.c
+++ src/main/c/com_github_marschall_getrandom_GetRandom.c
@@ -2,8 +2,14 @@
 
 #include <unistd.h>           /* for syscall() */
 #include <sys/syscall.h>      /* for __NR_* definitions */
+#include <stdlib.h>           /* for malloc and free */
 
 #include "com_github_marschall_getrandom_GetRandom.h"
+
+/*
+ * The maximum size of the stack-allocated buffer.
+ */
+#define BUFFER_SIZE 8192
 
 static inline ssize_t getrandom(void *buf, size_t buflen, unsigned int flags)
 {
@@ -13,5 +19,28 @@ static inline ssize_t getrandom(void *buf, size_t buflen, unsigned int flags)
 JNIEXPORT jint JNICALL Java_com_github_marschall_getrandom_GetRandom_getrandom0
   (JNIEnv *env, jclass clazz, jbyteArray bytes, jboolean r)
 {
+  _Static_assert (sizeof(jbyte) == sizeof(char), "sizeof(jbyte) == sizeof(char)");
+
+  jsize arrayLength = (*env)->GetArrayLength(env, bytes);
+  char stackBuffer[BUFFER_SIZE];
+  char *buffer = 0;
+  
+  if (arrayLength > BUFFER_SIZE)
+  {
+
+    buffer = malloc(sizeof(char) * (size_t) arrayLength);
+    if (buffer == NULL) {
+      return 0;
+    }
+  }
+  else
+  {
+    buffer = stackBuffer;
+  }
+  
+  if (arrayLength > BUFFER_SIZE)
+  {
+    free(buffer);
+  }
   return -1;
 }
