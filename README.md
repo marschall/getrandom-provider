b0f02526750544c6f1305aee1eacbc66b161354c
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-10 17:23:19 +0100

    Avoid emptying the entropy pool

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
+getrandom() SecureRandomSPI
+===========================
+
+A `SecureRandomSPI` that makes [getrandom()](http://man7.org/linux/man-pages/man2/getrandom.2.html) system call available to `SecureRandom`.
 
 
-Uses syscall, does not depend on glibc wrapper
-
-Tries to do stack allocation.
+* uses syscall, does not depend on glibc wrapper
+* tries to use stack allocation rather than allocation on the C heap
 
--- src/test/java/com/github/marschall/getrandom/GetrandomProviderTest.java
+++ src/test/java/com/github/marschall/getrandom/GetrandomProviderTest.java
@@ -1,11 +1,17 @@
 package com.github.marschall.getrandom;
 
+import static com.github.marschall.getrandom.GetrandomProviderTest.AllZeros.allZeros;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 import java.security.Security;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -28,59 +34,59 @@ class GetrandomProviderTest {
     SecureRandom secureRandom;
 
     secureRandom = SecureRandom.getInstance(GetrandomProvider.GETRANDOM);
-    verify(secureRandom);
+    verify(secureRandom, 16); // avoid emptying the entropy pool (/proc/sys/kernel/random/entropy_avail)
 
     secureRandom = SecureRandom.getInstance(GetrandomProvider.GETRANDOM, GetrandomProvider.NAME);
-    verify(secureRandom);
+    verify(secureRandom, 16); // avoid emptying the entropy pool (/proc/sys/kernel/random/entropy_avail)
   }
 
   @Test
   void geturandom() throws GeneralSecurityException {
     SecureRandom secureRandom;
 
-    secureRandom = SecureRandom.getInstance(GetrandomProvider.GETRANDOM);
-    verify(secureRandom);
+    secureRandom = SecureRandom.getInstance(GetrandomProvider.GETURANDOM);
+    verify(secureRandom, 128);
 
-    secureRandom = SecureRandom.getInstance(GetrandomProvider.GETRANDOM, GetrandomProvider.NAME);
-    verify(secureRandom);
+    secureRandom = SecureRandom.getInstance(GetrandomProvider.GETURANDOM, GetrandomProvider.NAME);
+    verify(secureRandom, 128);
   }
 
-  private static void verify(SecureRandom secureRandom) {
+  private static void verify(SecureRandom secureRandom, int poolSize) {
     assertNotNull(secureRandom);
 
-    byte[] buffer = new byte[128];
-//    assertThat(buffer, allZeros());
+    byte[] buffer = new byte[poolSize];
+    assertThat(buffer, allZeros());
 
     secureRandom.nextBytes(buffer);
-//    assertThat(buffer, not(allZeros()));
+    assertThat(buffer, not(allZeros()));
   }
 
-//  static final class AllZeros extends TypeSafeMatcher<byte[]> {
-//
-//    private AllZeros() {
-//      super();
-//    }
-//
-//    static Matcher<byte[]> allZeros() {
-//      return new AllZeros();
-//    }
-//
-//    @Override
-//    public void describeTo(Description description) {
-//      description.appendText("is all 0x00");
-//
-//    }
-//
-//    @Override
-//    protected boolean matchesSafely(byte[] item) {
-//      for (byte b : item) {
-//        if (b != 0) {
-//          return false;
-//        }
-//      }
-//      return true;
-//    }
-//
-//  }
+  static final class AllZeros extends TypeSafeMatcher<byte[]> {
+
+    private AllZeros() {
+      super();
+    }
+
+    static Matcher<byte[]> allZeros() {
+      return new AllZeros();
+    }
+
+    @Override
+    public void describeTo(Description description) {
+      description.appendText("is all 0x00");
+
+    }
+
+    @Override
+    protected boolean matchesSafely(byte[] item) {
+      for (byte b : item) {
+        if (b != 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+  }
 
 }
