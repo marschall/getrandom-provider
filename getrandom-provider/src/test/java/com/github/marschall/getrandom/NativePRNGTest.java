b968c15c2b9736924c88719fcbaa313e9af446d4
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-17 12:46:48 +0100

    Test for thread safety

--- getrandom-provider/src/test/java/com/github/marschall/getrandom/NativePRNGTest.java
+++ getrandom-provider/src/test/java/com/github/marschall/getrandom/NativePRNGTest.java
@@ -1,7 +1,11 @@
 package com.github.marschall.getrandom;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.security.GeneralSecurityException;
+import java.security.Provider;
 import java.security.SecureRandom;
+import java.security.Security;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +16,16 @@ class NativePRNGTest {
     SecureRandom random = SecureRandom.getInstance("NativePRNGNonBlocking", "SUN");
     byte[] bytes = new byte[32];
     random.nextBytes(bytes);
+  }
+
+  @Test
+  void treadSafe() throws GeneralSecurityException {
+    Provider sunProvider = Security.getProvider("SUN");
+    String algorithm = sunProvider.getProperty("SecureRandom.NativePRNGNonBlocking");
+    assertNotNull(algorithm);
+
+    String threadSafe = sunProvider.getProperty("SecureRandom.NativePRNGNonBlocking ThreadSafe", "false");
+    assertNotNull(threadSafe);
   }
 
 }
