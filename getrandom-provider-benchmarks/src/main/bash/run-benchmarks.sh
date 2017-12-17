d89c3401f152f9375d289bca94144e5ffc945f6e
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-17 15:19:31 +0100

    factor out commont code

--- getrandom-provider-benchmarks/src/main/bash/run-benchmarks.sh
+++ getrandom-provider-benchmarks/src/main/bash/run-benchmarks.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-$JAVA_HOME/bin/java \
- -XX:+UseParallelGC \
- -Xmx1g -Xms1g \
- -jar target/getrandom-provider-benchmarks-0.1.0-SNAPSHOT.jar \
- 1 threads-1.txt
+run_benchark() {
+   $JAVA_HOME/bin/java \
+     -XX:+UseParallelGC \
+     -Xmx1g -Xms1g \
+     -jar target/getrandom-provider-benchmarks-0.1.0-SNAPSHOT.jar \
+       $1 threads-$1.txt
+}
 
-# $JAVA_HOME/bin/java \
-#  -XX:+UseParallelGC \
-#  -Xmx1g -Xms1g \
-#  -jar target/getrandom-provider-benchmarks-1.0.0-SNAPSHOT.jar \
-#  2 threads-2.txt
-# 
-# $JAVA_HOME/bin/java \
-#  -XX:+UseParallelGC \
-#  -Xmx1g -Xms1g \
-#  -jar target/getrandom-provider-benchmarks-1.0.0-SNAPSHOT.jar \
-#  4 threads-4.txt
-# 
-# $JAVA_HOME/bin/java \
-#  -XX:+UseParallelGC \
-#  -Xmx1g -Xms1g \
-#  -jar target/getrandom-provider-benchmarks-1.0.0-SNAPSHOT.jar \
-#  8 threads-8.txt
+# run_benchark 1
+# run_benchark 2
+# run_benchark 4
+run_benchark 8
