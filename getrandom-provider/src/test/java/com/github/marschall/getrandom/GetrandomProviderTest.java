b3f8692bb7169788da6386eceef7f85b5439982c
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-24 17:54:32 +0100

    Provider autoregisters

--- getrandom-provider/src/test/java/com/github/marschall/getrandom/GetrandomProviderTest.java
+++ getrandom-provider/src/test/java/com/github/marschall/getrandom/GetrandomProviderTest.java
@@ -7,23 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
-import java.security.Security;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class GetrandomProviderTest {
-
-  @BeforeAll
-  static void addProvider() {
-    Security.addProvider(new GetrandomProvider());
-  }
-
-  @AfterAll
-  static void installProvider() {
-    Security.removeProvider(GetrandomProvider.NAME);
-  }
 
   @Test
   void getrandom() throws GeneralSecurityException {
